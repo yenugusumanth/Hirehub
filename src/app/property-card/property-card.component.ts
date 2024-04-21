3e02f965b568ad33017ce376f6a4bf22c7f268af
sumanth <yenugusumanth110@gmail.com>
2024-04-21 16:29:01 -0500

    components

---
+++ src/app/property-card/property-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './property-card.component.html',
+  styleUrl: './property-card.component.css'
+})
+export class PropertyCardComponent {
+
+}
